b57d98bf2e4a07f535c2fa88314aa2d5142246bb
arthurx034 <1008188@senacminas.edu.br>
2025-06-12 16:54:00 -0300

    AplicativoFinalizado.

---
+++ app/src/main/java/com/arthur/aplicativodecursos/view/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.arthur.aplicativodecursos.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.aplicativodecursos.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        comutartelaSplash();
+    }
+
+    private void comutartelaSplash() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent telaPrincipal = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(telaPrincipal);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
